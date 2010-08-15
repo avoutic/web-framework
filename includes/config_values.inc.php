67e6de139b3f6f26bf7178d6fec6db664cc0f117
root <root@70927db1-c6f8-4d3c-a5c6-6742c237d500>
2010-08-15 19:02:54 +0000

    - Added UserConfigValues

--- includes/config_values.inc.php
+++ includes/config_values.inc.php
@@ -32,8 +32,51 @@ class ConfigValues
         if ($module == "")
             $module == $this->default_module;
 
-        $result = $this->database->Query('REPLACE INTO config_values SET module = ?, name = ?, value = ?',
-            array($module, $name, $value));
+        $result = $this->database->Query('INSERT INTO config_values SET module = ?, name = ?, value = ? ON DUPLICATE KEY UPDATE value = ?',
+            array($module, $name, $value, $value));
+
+        if ($result === FALSE)
+            die('Failed to store config value.');
+    }
+};
+
+class UserConfigValues
+{
+    private $database;
+    private $user_id;
+    private $default_module;
+
+    function __construct($database, $user_id, $default_module = "")
+    {
+        $this->database = $database;
+        $this->user_id = $user_id;
+        $this->default_module = $default_module;
+    }
+
+    function get_value($name, $module = "")
+    {
+        if ($module == "")
+            $module == $this->default_module;
+
+        $result = $this->database->Query('SELECT value FROM user_config_values WHERE user_id = ? AND module = ? AND name = ?',
+            array($this->user_id, $module, $name));
+
+        if ($result === FALSE)
+            die('Failed to retrieve config value.');
+
+        if ($result->RecordCount() != 1)
+            return "";
+
+        return $result->fields['value'];
+    }
+
+    function set_value($name, $value, $module = "")
+    {
+        if ($module == "")
+            $module == $this->default_module;
+
+        $result = $this->database->Query('INSERT user_config_values SET user_id = ?, module = ?, name = ?, value = ? ON DUPLICATE KEY UPDATE value = ?',
+            array($this->user_id, $module, $name, $value, $value));
 
         if ($result === FALSE)
             die('Failed to store config value.');
