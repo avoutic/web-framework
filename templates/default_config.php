62d03fc0cbfd3890fc509af15452a386a1c097ea
Avoutic <avoutic@gmail.com>
2012-06-24 14:31:17 +0200

    Proper database configureation and name change

---
+++ templates/default_config.php
@@ -0,0 +1,18 @@
+<?php
+
+$site_config = array(
+    'database_enabled' => true,
+    'database' => array(
+        'database_type' => 'mysql',
+        'database_host' => 'localhost',
+        'database_user' => 'USER',
+        'database_password' => 'PASSWORD',
+        'database_database' => 'DATABASE',
+    ),
+    'debug' => true,
+    'page' => array(
+        'default_frame_file' => 'default_frame.inc.php'
+    )
+);
+
+?>
