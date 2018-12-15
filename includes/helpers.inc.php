22d91254faa823e7e8f9e6fd2820bb3490f4e9df
Avoutic <avoutic@gmail.com>
2018-12-15 07:50:33 -0500

    Add time_elapsed_string helper function

--- includes/helpers.inc.php
+++ includes/helpers.inc.php
@@ -131,4 +131,38 @@ class Helpers
                 $ftime['tm_yday'] + 1,
                 $ftime['tm_year'] + 1900);
     }
+
+    public static function time_elapsed_string($ptime)
+    {
+        $etime = time() - $ptime;
+
+        if ($etime < 1)
+            return '0 seconds';
+
+        $a = array( 365 * 24 * 60 * 60  =>  'year',
+                    30 * 24 * 60 * 60  =>  'month',
+                    24 * 60 * 60  =>  'day',
+                    60 * 60  =>  'hour',
+                    60  =>  'minute',
+                    1  =>  'second'
+                );
+        $a_plural = array( 'year'   => 'years',
+                           'month'  => 'months',
+                           'day'    => 'days',
+                           'hour'   => 'hours',
+                           'minute' => 'minutes',
+                           'second' => 'seconds'
+                );
+
+        foreach ($a as $secs => $str)
+        {
+            $d = $etime / $secs;
+
+            if ($d >= 1)
+            {
+                $r = round($d);
+                return $r . ' ' . ($r > 1 ? $a_plural[$str] : $str);
+            }
+        }
+    }
 };
