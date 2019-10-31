50b17108fb6993f6e1bd8125a6f5f06cd28781ab
Avoutic <avoutic@gmail.com>
2019-10-31 16:25:51 +0100

    Add title to Hello World example

--- examples/main.inc.php
+++ examples/main.inc.php
@@ -1,6 +1,11 @@
 <?php
 class PageMain extends PageBasic
 {
+    function get_title()
+    {
+        return 'Hello World';
+    }
+
     function display_content()
     {
         $this->load_template('main.tpl', $this->page_content);
