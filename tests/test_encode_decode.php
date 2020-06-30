562e63b81ead1513696d04e167c83c497c4a5cad
Avoutic <avoutic@gmail.com>
2020-06-30 14:14:31 +0200

    Update test script to current framework version

--- tests/test_encode_decode.php
+++ tests/test_encode_decode.php
@@ -1,22 +1,19 @@
 <?php
 # Global configuration
 #
-$includes=__DIR__.'/../includes/';
-$site_includes=__DIR__.'/includes/';
-$site_views=__DIR__.'/../views/';
-$site_frames=__DIR__.'/../frames/';
-$site_templates=__DIR__.'/../templates/';
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../includes/wf_core.inc.php');
 
-require_once($includes.'wf_core.inc.php');
+$framework = new WF();
+$framework->init();
+$security = $framework->get_security();
 
 $msg = 'This is a message I need to encrypt, store, decode and verify!';
 
-$encoded = encode_and_auth_string($msg);
+$encoded = $security->encode_and_auth_array(array('msg' => $msg));
 
 print('Encoded: '.$encoded.PHP_EOL);
 
-$encoded = urldecode($encoded);
-$decoded = decode_and_verify_string($encoded);
+$decoded = $security->decode_and_verify_array($encoded);
 
-print_r('Decoded: '.$decoded.PHP_EOL);
+print_r('Decoded: '.$decoded['msg'].PHP_EOL);
