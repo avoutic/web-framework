b8e98582f61cb40aac8c4b8dff0324f8c3f20154
Avoutic <avoutic@gmail.com>
2020-08-04 13:00:09 +0200

    Allow setting of secret_key and retrieving error codes in Recaptcha

--- includes/recaptcha.inc.php
+++ includes/recaptcha.inc.php
@@ -1,6 +1,9 @@
 <?php
 class Recaptcha extends FrameworkCore
 {
+    protected $secret_key;
+    protected $error_codes = array();
+
     function __construct()
     {
         parent::__construct();
@@ -9,6 +12,18 @@ class Recaptcha extends FrameworkCore
 
         $this->verify(strlen($this->module_config['site_key']), 'Missing reCAPTCHA Site Key');
         $this->verify(strlen($this->module_config['secret_key']), 'Missing reCAPTCHA Secret Key');
+
+        $this->secret_key = $this->module_config['secret_key'];
+    }
+
+    function set_secret_key($secret_key)
+    {
+        $this->secret_key = $secret_key;
+    }
+
+    function get_error_codes()
+    {
+        return $this->error_codes;
     }
 
     function verify_response($recaptcha_response)
@@ -16,7 +31,8 @@ class Recaptcha extends FrameworkCore
         if (!strlen($recaptcha_response))
             return false;
 
-        $recaptcha_secret = $this->module_config['secret_key'];
+        $recaptcha_secret = $this->secret_key;
+        $this->error_codes = array();
 
         $recaptcha_data = array(
             'secret' => $recaptcha_secret,
@@ -35,6 +51,8 @@ class Recaptcha extends FrameworkCore
 
         if (isset($response['error_codes']))
         {
+            $this->error_codes = $response['error_codes'];
+
             $this->verify(!in_array('invalid-input-secret', $response['error-codes']), 'Invalid reCAPTCHA input secret used');
             $this->verify(!in_array('invalid-keys-secret', $response['error-codes']), 'Invalid reCAPTCHA key used');
         }
