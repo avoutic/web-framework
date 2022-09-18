a092dd8a501b37ddab57e6c5e55f1496c297244d
Avoutic <avoutic@gmail.com>
2022-09-18 17:27:49 +0200

    Example has moved to own repository
    Can now be found at:
    https://github.com/avoutic/web-framework-example

--- hello_world_example/includes/auth/db_config.main.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'database_type' => 'mysqli',
-    'database_host' => 'HOSTNAME',
-    'database_user' => 'USER',
-    'database_password' => 'PASSWORD',
-    'database_database' => 'DB',
-);
-?>
--- hello_world_example/includes/site_logic.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Example:
-// $framework->register_route('GET /something', 'something_file', 'class.function', array('optional_arg_names_to_map_to'));
-
-$framework->register_route('GET /main', 'main', 'PageMain.html_main');
-?>
