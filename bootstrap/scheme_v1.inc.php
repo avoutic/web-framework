cb8502ed03acf62670dd9d698a94ed73b1da5346
Avoutic <avoutic@gmail.comi>
2022-10-09 14:41:11 +0200

    Add db_init.php script to initialize database via framework

---
+++ bootstrap/scheme_v1.inc.php
@@ -0,0 +1,259 @@
+<?php
+return [
+    'target_version' => 1,
+    'actions' => [
+        [
+            'type' => 'create_table',
+            'table_name' => 'blacklist_entries',
+            'fields' => [
+                [
+                    'name' => 'ip',
+                    'type' => 'varchar',
+                    'size' => 40,
+                ],
+                [
+                    'name' => 'user_id',
+                    'type' => 'int',
+                    'null' => true,
+                ],
+                [
+                    'name' => 'severity',
+                    'type' => 'int',
+                ],
+                [
+                    'name' => 'reason',
+                    'type' => 'varchar',
+                    'size' => 100,
+                ],
+                [
+                    'name' => 'timestamp',
+                    'type' => 'int',
+                ],
+            ],
+            'constraints' => [
+            ],
+        ],
+        [
+            'type' => 'create_table',
+            'table_name' => 'rights',
+            'fields' => [
+                [
+                    'name' => 'short_name',
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                [
+                    'name' => 'name',
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+            ],
+            'constraints' => [
+            ],
+        ],
+        [
+            'type' => 'create_table',
+            'table_name' => 'users',
+            'fields' => [
+                [
+                    'name' => 'username',
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                [
+                    'name' => 'solid_password',
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                [
+                    'name' => 'name',
+                    'type' => 'varchar',
+                    'size' => 255,
+                    'null' => true,
+                    'default' => '',
+                ],
+                [
+                    'name' => 'email',
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                [
+                    'name' => 'registered',
+                    'type' => 'int',
+                ],
+                [
+                    'name' => 'verified',
+                    'type' => 'boolean',
+                    'default' => '0',
+                ],
+                [
+                    'name' => 'failed_login',
+                    'type' => 'int',
+                    'default' => '0',
+                ],
+                [
+                    'name' => 'terms_accepted',
+                    'type' => 'int',
+                    'default' => '0',
+                ],
+                [
+                    'name' => 'last_login',
+                    'type' => 'int',
+                    'default' => '0',
+                ],
+            ],
+            'constraints' => [
+                [
+                    'type' => 'unique',
+                    'values' => [ 'username' ],
+                ],
+            ],
+        ],
+        [
+            'type' => 'create_table',
+            'table_name' => 'user_rights',
+            'fields' => [
+                [
+                    'name' => 'user_id',
+                    'type' => 'foreign_key',
+                    'foreign_table' => 'users',
+                    'on_delete' => 'cascade',
+                    'on_update' => 'cascade',
+                    'foreign_field' => 'id',
+                ],
+                [
+                    'name' => 'right_id',
+                    'type' => 'foreign_key',
+                    'foreign_table' => 'rights',
+                    'foreign_field' => 'id',
+                    'on_delete' => 'cascade',
+                    'on_update' => 'cascade',
+                ],
+            ],
+            'constraints' => [
+                [
+                    'type' => 'unique',
+                    'values' => [ 'user_id', 'right_id' ],
+                ],
+            ],
+        ],
+        [
+            'type' => 'create_table',
+            'table_name' => 'sessions',
+            'fields' => [
+                [
+                    'name' => 'user_id',
+                    'type' => 'foreign_key',
+                    'foreign_table' => 'users',
+                    'on_delete' => 'cascade',
+                    'on_update' => 'cascade',
+                    'foreign_field' => 'id',
+                ],
+                [
+                    'name' => 'session_id',
+                    'type' => 'varchar',
+                    'size' => 255,
+                ],
+                [
+                    'name' => 'start',
+                    'type' => 'datetime',
+                    'default' => 'CURRENT_TIMESTAMP',
+                ],
+                [
+                    'name' => 'last_active',
+                    'type' => 'datetime',
+                ],
+            ],
+            'constraints' => [
+            ],
+        ],
+        [
+            'type' => 'create_table',
+            'table_name' => 'config_values',
+            'fields' => [
+                [
+                    'name' => 'module',
+                    'type' => 'varchar',
+                    'size' => 45,
+                ],
+                [
+                    'name' => 'name',
+                    'type' => 'varchar',
+                    'size' => 45,
+                ],
+                [
+                    'name' => 'value',
+                    'type' => 'varchar',
+                    'size' => 45,
+                ],
+            ],
+            'constraints' => [
+                [
+                    'type' => 'unique',
+                    'values' => [ 'module', 'name' ],
+                ],
+            ],
+        ],
+        [
+            'type' => 'create_table',
+            'table_name' => 'user_config_values',
+            'fields' => [
+                [
+                    'name' => 'user_id',
+                    'type' => 'foreign_key',
+                    'foreign_table' => 'users',
+                    'on_delete' => 'cascade',
+                    'on_update' => 'cascade',
+                    'foreign_field' => 'id',
+                ],
+                [
+                    'name' => 'module',
+                    'type' => 'varchar',
+                    'size' => 45,
+                ],
+                [
+                    'name' => 'name',
+                    'type' => 'varchar',
+                    'size' => 45,
+                ],
+                [
+                    'name' => 'value',
+                    'type' => 'varchar',
+                    'size' => 45,
+                ],
+            ],
+            'constraints' => [
+                [
+                    'type' => 'unique',
+                    'values' => [ 'user_id', 'module', 'name' ],
+                ],
+            ],
+        ],
+        [
+            'type' => 'insert_row',
+            'table_name' => 'config_values',
+            'values' => [
+                'module' => 'db',
+                'name' => 'wf_db_version',
+                'value' => '2',
+            ],
+        ],
+        [
+            'type' => 'insert_row',
+            'table_name' => 'config_values',
+            'values' => [
+                'module' => 'db',
+                'name' => 'app_db_version',
+                'value' => '0',
+            ],
+        ],
+        [
+            'type' => 'insert_row',
+            'table_name' => 'rights',
+            'values' => [
+                'short_name' => 'admin',
+                'name' => 'Admin',
+            ],
+        ],
+    ],
+];
---
+++ scripts/db_init.php
@@ -0,0 +1,61 @@
+<?php
+
+// Global configuration
+//
+if (!file_exists(__DIR__.'/../vendor/autoload.php'))
+{
+    exit('Composer not initialized');
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use WebFramework\Core\DatabaseManager;
+use WebFramework\Core\WF;
+
+$framework = new WF();
+
+try
+{
+    // Initialize WF
+    //
+    $framework->skip_db_check();
+    $framework->init();
+    $app_dir = $framework->get_app_dir();
+
+    $db_manager = new DatabaseManager();
+
+    if ($db_manager->is_initialized())
+    {
+        echo " - Already initialized. Exiting.".PHP_EOL;
+
+        exit();
+    }
+
+    $scheme_file = "{$app_dir}/vendor/avoutic/web-framework/bootstrap/scheme_v1.inc.php";
+
+    if (!file_exists($scheme_file))
+    {
+        echo " - Scheme file {$scheme_file} not found".PHP_EOL;
+
+        exit();
+    }
+
+    $change_set = require $scheme_file;
+    WF::verify(is_array($change_set), 'No change set array found');
+
+    $db_manager->execute($change_set, true);
+}
+catch (Throwable $e)
+{
+    echo('Unhandled exception');
+
+    if ($framework->get_config('debug') == true)
+    {
+        echo($e->getMessage().PHP_EOL);
+        print_r($e->getTrace());
+    }
+
+    WF::report_error($e->getMessage(), $e->getTrace());
+
+    exit();
+}
