4f15e70d2660c3c9269cc995506e7270caf4033e
Avoutic <avoutic@gmail.comi>
2023-02-01 23:12:40 +0100

    Add ~ to FORMAT_RETURN_QUERY

--- includes/defines.inc.php
+++ includes/defines.inc.php
@@ -14,7 +14,7 @@ define('FORMAT_NAME', '[\w _\-\.\']+');
 define('FORMAT_EMAIL', '[\w\._\-+]+@[\w_\-\.]+\.[\w_\-]+');
 define('FORMAT_VERIFY_CODE', '[\w]{40}');
 define('FORMAT_RETURN_PAGE', '[\/\w\.\-_]+');
-define('FORMAT_RETURN_QUERY', '[\w\&=:\._\-%\(\)\?\/]+');
+define('FORMAT_RETURN_QUERY', '[\w\&~=:\._\-%\(\)\?\/]+');
 define('FORMAT_FILE_NAME', '[\w_\-\.]+');
 define('FORMAT_FILE_LOCATION', '[\w_\-\.\/]+');
 define('FORMAT_BASE64', '[a-zA-Z0-9+\/]+={0,2}');
