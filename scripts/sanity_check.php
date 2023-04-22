9333c57f719bda278d2c395f5037c6f0f1c7d37b
Avoutic <avoutic@gmail.comi>
2023-04-22 14:35:46 +0200

    Add Sanity Check script

---
+++ scripts/sanity_check.php
@@ -0,0 +1,48 @@
+<?php
+// Global configuration
+//
+if (!file_exists(__DIR__.'/../vendor/autoload.php'))
+{
+    exit('Composer not initialized');
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use WebFramework\Core\WF;
+
+// Initialize WF
+//
+$framework = new WF();
+
+try
+{
+    // Initialize WF
+    //
+    $framework->init();
+
+    header("content-type: text/plain");
+
+    $class_names = $framework->get_sanity_checks_to_run();
+
+    foreach ($class_names as $class_name => $module_config)
+    {
+        $sanity_check = $framework->instantiate_sanity_check($class_name, $module_config);
+        $sanity_check->allow_fixing();
+        $sanity_check->set_verbose();
+        $sanity_check->perform_checks();
+    }
+}
+catch (Throwable $e)
+{
+    print('Unhandled exception'.PHP_EOL);
+
+    if ($framework->get_config('debug') == true)
+    {
+        print($e->getMessage().PHP_EOL);
+        print_r($e->getTrace());
+    }
+
+    WF::report_error($e->getMessage(), $e->getTrace());
+    exit();
+}
+?>
