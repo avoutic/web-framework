be02fdca4649c177a59156292d90859c2ba414d3
Avoutic <avoutic@gmail.comi>
2023-04-22 14:24:54 +0200

    Add SanityCheck base class and RequiredAuth module

---
+++ includes/SanityCheck/Base.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WebFramework\Core\SanityCheck;
+
+use WebFramework\Core\SanityCheckInterface;
+use WebFramework\Core\WF;
+
+abstract class Base implements SanityCheckInterface
+{
+    protected bool $allow_fixing = false;
+    protected bool $verbose = false;
+    protected array $config;
+
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    protected function get_app_dir(): string
+    {
+        // Determine app dir
+        //
+        $reflection = new \ReflectionClass(\Composer\Autoload\ClassLoader::class);
+
+        $filename = $reflection->getFileName();
+        WF::verify($filename !== false, 'Failed to retrieve filename');
+
+        return dirname($filename, 3);
+    }
+
+    public abstract function perform_checks(): bool;
+
+    public function allow_fixing(): void
+    {
+        $this->allow_fixing = true;
+    }
+
+    public function set_verbose(): void
+    {
+        $this->verbose = true;
+    }
+
+    protected function add_output(string $str): void
+    {
+        if ($this->verbose)
+        {
+            echo $str;
+        }
+    }
+}
---
+++ includes/SanityCheck/RequiredAuth.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace WebFramework\Core\SanityCheck;
+
+use WebFramework\Core\WF;
+
+class RequiredAuth extends Base
+{
+    public function perform_checks(): bool
+    {
+        // Check if all required auth files are present
+        //
+        $this->add_output('Checking for auths:'.PHP_EOL);
+
+        $required_auths = $this->config;
+
+        $error = false;
+        $app_dir = $this->get_app_dir();
+
+        foreach ($required_auths as $filename)
+        {
+            $path = "{$app_dir}/includes/auth/{$filename}";
+
+            $exists = file_exists($path);
+
+            if ($exists)
+            {
+                $this->add_output(" - {$filename} present".PHP_EOL);
+                continue;
+            }
+
+            $this->add_output(" - {$filename} not present. Not fixing.".PHP_EOL);
+            $error = true;
+        }
+
+        if (!$error)
+        {
+            $this->add_output('  Pass'.PHP_EOL.PHP_EOL);
+        }
+        else
+        {
+            $this->add_output(PHP_EOL.'Breaking off');
+            return false;
+        }
+
+        return true;
+    }
+}
