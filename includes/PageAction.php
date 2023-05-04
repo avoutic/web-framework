a0828cde0e4d256a392cccefbc68ccba4aefd4da
Avoutic <avoutic@gmail.comi>
2023-05-04 13:03:27 +0200

    Introduce support for latte templating system in PageAction

--- includes/PageAction.php
+++ includes/PageAction.php
@@ -61,6 +61,18 @@ abstract class PageAction extends ActionCore
         return 'index,follow';
     }
 
+    protected function get_template_system(): string
+    {
+        $template_file = $this->get_template_file();
+
+        if (substr($template_file, -6) === '.latte')
+        {
+            return 'latte';
+        }
+
+        return 'native';
+    }
+
     protected function get_frame_file(): string
     {
         return $this->frame_file;
@@ -100,9 +112,79 @@ abstract class PageAction extends ActionCore
 
     protected function display_content(): void
     {
+        $template_file = $this->get_template_file();
+
+        $this->load_template($template_file, $this->page_content);
+    }
+
+    protected function get_template_file(): string
+    {
+        return '';
     }
 
     protected function display_frame(): void
+    {
+        $template_system = $this->get_template_system();
+
+        if ($template_system === 'latte')
+        {
+            $this->display_with_latte();
+            return;
+        }
+        elseif ($template_system === 'native')
+        {
+            $this->display_with_native();
+            return;
+        }
+
+        $this->verify(false, 'Not a supported template_system: '.$template_system);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    protected function get_core_variables(): array
+    {
+        $data = [];
+
+        $data['base_url'] = $this->get_base_url();
+        $data['content_title'] = $this->get_content_title();
+        $data['csrf_token'] = $this->get_csrf_token();
+        $data['canonical'] = $this->get_canonical();
+        $data['description'] = $this->get_description();
+        $data['keywords'] = $this->get_keywords();
+        $data['meta_robots'] = $this->get_meta_robots();
+        $data['title'] = $this->get_title();
+
+        $data['build_info'] = $this->get_build_info();
+        $data['config'] = $this->get_config('');
+        $data['messages'] = $this->get_messages();
+
+        return $data;
+    }
+
+    protected function display_with_latte(): void
+    {
+        $app_dir = $this->get_app_dir();
+        $template_dir = "{$app_dir}/templates";
+
+        $this->page_content['core'] = $this->get_core_variables();
+
+        $latte = new \Latte\Engine;
+        $latte->setTempDirectory('/tmp/latte');
+        $latte->setLoader(new \Latte\Loaders\FileLoader($template_dir));
+
+        $template_file = $this->get_template_file();
+
+        $this->verify(file_exists("{$template_dir}/{$template_file}"), 'Requested template not present');
+
+        $output = $latte->renderToString($template_file, $this->page_content);
+
+        echo($output);
+        return;
+    }
+
+    protected function display_with_native(): void
     {
         // Unset availability of input in display
         // Forces explicit handling in do_logic()
