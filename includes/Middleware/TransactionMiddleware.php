686569e9b2e9527315faaae6ce8b4146b81b9c2c
Avoutic <avoutic@gmail.comi>
2023-06-13 11:16:40 +0200

    Add TransactionMiddleware to only commit on success

---
+++ includes/Middleware/TransactionMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WebFramework\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use WebFramework\Core\Database;
+
+class TransactionMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private Database $database,
+    ) {
+    }
+
+    public function process(Request $request, RequestHandlerInterface $handler): Response
+    {
+        $this->database->startTransaction();
+
+        $response = $handler->handle($request);
+
+        $this->database->commitTransaction();
+
+        return $response;
+    }
+}
