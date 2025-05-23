cd722c2ab4e8b9e683bda0956573482e1a1df8e2
Avoutic <avoutic@gmail.com>
2025-05-23 20:59:18 +0200

    Remove unused defines

--- src/Defines.php
+++ src/Defines.php
@@ -17,13 +17,4 @@ define('FRAMEWORK_DB_VERSION', 3);
 //
 define('FORMAT_ID', '\d+');
 define('FORMAT_USERNAME', '[\w_\-\.]+');
-define('FORMAT_PASSWORD', '.*');
-define('FORMAT_NAME', '[\w _\-\.\']+');
-define('FORMAT_EMAIL', '[\w\._\-+]+@[\w_\-\.]+\.[\w_\-]+');
-define('FORMAT_VERIFY_CODE', '[\w]{40}');
 define('FORMAT_RETURN_PAGE', '[\/\w\.\-_]+');
-define('FORMAT_RETURN_QUERY', '[\w\&~=:\._\-%\(\)\?\/]+');
-define('FORMAT_FILE_NAME', '[\w_\-\.]+');
-define('FORMAT_FILE_LOCATION', '[\w_\-\.\/]+');
-define('FORMAT_BASE64', '[a-zA-Z0-9+\/]+={0,2}');
-define('CHAR_FILTER', '"*&<>;\'');
